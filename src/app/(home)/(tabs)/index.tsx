da2a20474c057085ecacf86289f5e8d4e09d29ad
MURAT CANKAT KÖK <mcankatkok@gmail.com>
2024-09-20 02:09:36 +0300

    List of users

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,14 +1,30 @@
-import { router } from "expo-router";
-
+import { Link, router, Stack } from "expo-router";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { ChannelList } from "stream-chat-expo";
 import { useAuth } from "../../../providers/AuthProvider";
 
 export default function MainTabScreen() {
-  const {user} = useAuth();
+  const { user } = useAuth();
   return (
-    <ChannelList
-      filters={{members: {$in:[user.id]}}}
-      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-    />
+    <>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <Link href={"/(home)/users"} asChild>
+              <FontAwesome6
+                name="users"
+                size={22}
+                color="grey"
+                style={{ marginHorizontal: 16 }}
+              />
+            </Link>
+          ),
+        }}
+      />
+      <ChannelList
+        filters={{ members: { $in: [user.id] } }}
+        onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+      />
+    </>
   );
 }
